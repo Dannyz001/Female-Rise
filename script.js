264415f9e2a7ee1a11daf1d960f4a62e3523597f
Dannyz001 <122610632+Dannyz001@users.noreply.github.com>
2023-12-17 22:25:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const linkContainer = document.querySelector('.link-container');
+const navBar = document.querySelector('.nav-bar')
 
 
 hamburger.addEventListener('click', ()=> {
     linkContainer.classList.toggle('active');
+    navBar.classList.toggle('fixed-nav')
 });
